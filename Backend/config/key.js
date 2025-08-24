17dc9c82f04666d14c6530163f4b9eb4812e0012
UBU11 <uvaisubu11@gmail.com>
2025-08-24 20:38:07 +0530

    authMiddleware for user check logged or not

---
+++ Backend/config/key.js
@@ -0,0 +1,5 @@
+MONGO_URI = your_mongodb_connection_string;
+GOOGLE_CLIENT_ID = your_google_client_id;
+GOOGLE_CLIENT_SECRET = your_google_client_secret;
+COOKIE_KEY = a_long_random_string_for_cookie_encryption;
+PORT = 5000;
---
+++ Backend/middleware/requireAuth.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).send({ error: "You must be logged in." });
+  }
+  next();
+};
