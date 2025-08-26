26e735f66237b0d1fa8a3e631b29797f8525d1ce
UBU11 <uvaisubu11@gmail.com>
2025-08-26 21:13:33 +0530

    feat: button is created

---
+++ Client/src/Components/common/Button.jsx
@@ -0,0 +1,30 @@
+
+const Button = ({ children, onClick, type = "button" }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className="
+        px-6
+        py-3
+        font-semibold
+        text-white
+        bg-indigo-600
+        rounded-lg
+        shadow-md
+        transition-all
+        duration-300
+        ease-in-out
+        hover:bg-indigo-700
+        hover:-translate-y-1
+        hover:shadow-lg
+        active:scale-95
+        active:shadow-sm
+      "
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
